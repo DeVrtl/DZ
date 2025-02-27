c01414ee246b9e97b74b2a3f9800c05b2554d888
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2025-02-27 22:17:53 +0500

    New homework completed

--- Assets/Script/SecondDZ/LoggerWrapper.cs
+++ Assets/Script/SecondDZ/LoggerWrapper.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LoggerWrapper : MonoBehaviour
 { 
-   [SerializeField] private UnityLogger logger = new();
+   [SerializeField] private UnityLogger logger;
 
    public UnityLogger Logger => logger;
 
@@ -15,9 +15,9 @@ public class LoggerWrapper : MonoBehaviour
          List<string> stringsList = new List<string>() { "STR", "StringTwo", "StringThree" };
          string[] stringsArray = new string[] { "STRARR", "StringArrTwo" };
          IEnumerable<string> onlyUpperCase = stringsList.Where(str => str.All(ch => char.IsUpper(ch))); // выбираем те строки, где символы в upperCase
-         Logger.LogCollection(stringsList, s => {logger.Log(s);});
-         Logger.LogCollection(stringsArray, s => {logger.Log(s);});
-         Logger.LogCollection(onlyUpperCase, s => {logger.Log(s);});
+         Logger.LogCollection(stringsList, s => logger.Log(s));
+         Logger.LogCollection(stringsArray, s => logger.Log(s));
+         Logger.LogCollection(onlyUpperCase, s => logger.Log(s));
       }
    }
 }
